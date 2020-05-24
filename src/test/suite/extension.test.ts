887ffe9b01b55ac94aef6d19e50755650ea7f61e
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-05-24 14:38:40 +0200

    Add some basic tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,36 @@
-import * as assert from 'assert';
+import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as ast from "./ast.json";
+import { findLinks, findTitle, id } from "../../utils";
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite("Tests", () => {
+  vscode.window.showInformationMessage("Start all tests.");
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
+  test("findLinks works", () => {
+    const links = findLinks(ast);
+    const expected = [
+      "another.md",
+      "other.md",
+      "nested.md",
+      "error.md",
+      "more.md",
+    ];
+
+    assert.deepStrictEqual(links, expected);
+  });
+
+  test("findTitle works", () => {
+    assert.equal(findTitle(ast), "Links");
+  });
+
+  test("id works", () => {
+    assert.equal(
+      id("/Users/example/Desktop/notes/1.md"),
+      "daea8e534ce10d7a2e16635f182c29b8"
+    );
+  });
 });
