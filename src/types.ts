bbac752aaed1fa2ace10ae586f7a795c6f44bc02
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-05-24 03:55:24 +0200

    Extract types to separate file

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export type Edge = {
+  source: string;
+  target: string;
+};
+
+export type Node = {
+  id: string;
+  path: string;
+  label: string;
+};
+
+export type MarkdownNode = {
+  type: string;
+  children?: MarkdownNode[];
+  url?: string;
+  value?: string;
+  depth?: number;
+  data?: {
+    permalink?: string;
+  };
+};
