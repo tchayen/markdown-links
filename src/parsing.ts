3771e52e2e86e5c7211c9742e8e3f3b733938ea4
ingalless <jonathanaingall@gmail.com>
2020-08-13 13:32:49 +0100

    Use workspace.findFiles to respect ignored files (#35)
    * Use workspace.findFiles to respect ignored files
    
    * Glob file extension, not just "md" in file name
    
    * Include comment about use of findFiles
    
    Co-authored-by: Tomasz Czajęcki <tomek.czajecki@gmail.com>
    
    * Fix filepaths on Windows
    
    Co-authored-by: Jonathan Ingall <jonny@mondago.com>
    Co-authored-by: Tomasz Czajęcki <tomek.czajecki@gmail.com>

--- src/parsing.ts
+++ src/parsing.ts
@@ -6,7 +6,14 @@ import * as wikiLinkPlugin from "remark-wiki-link";
 import * as frontmatter from "remark-frontmatter";
 import { MarkdownNode, Graph } from "./types";
 import { TextDecoder } from "util";
-import { findTitle, findLinks, id, FILE_ID_REGEXP, getFileTypesSetting, getConfiguration } from "./utils";
+import {
+  findTitle,
+  findLinks,
+  id,
+  FILE_ID_REGEXP,
+  getFileTypesSetting,
+  getConfiguration,
+} from "./utils";
 import { basename } from "path";
 
 let idToPath: Record<string, string> = {};
@@ -26,6 +33,7 @@ const parser = unified()
   .use(frontmatter);
 
 export const parseFile = async (graph: Graph, filePath: string) => {
+  filePath = path.normalize(filePath);
   const buffer = await vscode.workspace.fs.readFile(vscode.Uri.file(filePath));
   const content = new TextDecoder("utf-8").decode(buffer);
   const ast: MarkdownNode = parser.parse(content);
@@ -52,10 +60,10 @@ export const parseFile = async (graph: Graph, filePath: string) => {
   graph.edges = graph.edges.filter((edge) => edge.source !== id(filePath));
 
   const links = findLinks(ast);
-  const parentDirectory = filePath.split("/").slice(0, -1).join("/");
+  const parentDirectory = filePath.split(path.sep).slice(0, -1).join(path.sep);
 
   for (const link of links) {
-    let target = link;
+    let target = path.normalize(link);
     if (!path.isAbsolute(link)) {
       target = path.normalize(`${parentDirectory}/${link}`);
     }
@@ -87,31 +95,20 @@ export const learnFileId = async (_graph: Graph, filePath: string) => {
 
 export const parseDirectory = async (
   graph: Graph,
-  directory: string,
   fileCallback: (graph: Graph, path: string) => Promise<void>
 ) => {
-  const files = await vscode.workspace.fs.readDirectory(
-    vscode.Uri.file(directory)
+  // `findFiles` is used here since it respects files excluded by either the
+  // global or workspace level files.exclude config option.
+  const files = await vscode.workspace.findFiles(
+    `**/*{${(getFileTypesSetting() as string[]).map((f) => `.${f}`).join(",")}}`
   );
 
   const promises: Promise<void>[] = [];
 
   for (const file of files) {
-    const fileName = file[0];
-    const fileType = file[1];
-    const isDirectory = fileType === vscode.FileType.Directory;
-    const isFile = fileType === vscode.FileType.File;
-    const hiddenFile = fileName.startsWith(".");
-    const isGraphFile = getFileTypesSetting().includes(
-      fileName.substr(fileName.lastIndexOf('.') + 1)
-    )
-
-    if (isDirectory && !hiddenFile) {
-      promises.push(
-        parseDirectory(graph, `${directory}/${fileName}`, fileCallback)
-      );
-    } else if (isFile && isGraphFile) {
-      promises.push(fileCallback(graph, `${directory}/${fileName}`));
+    const hiddenFile = path.basename(file.path).startsWith(".");
+    if (!hiddenFile) {
+      promises.push(fileCallback(graph, file.path));
     }
   }
 
